83a2811ccb96d4467ee20c416b9bbd8e22e3f24e
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-12-08 22:54:05 -0300

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -2,26 +2,6 @@ using System.Diagnostics;
 
 static class Program
 {
-  // Método utilizado para exportar os grafos para um arquivo.
-  // static void Export()
-  // {
-  //   Console.WriteLine("Exporting graphs...");
-
-  //   for (int i = 10; i < 100_001; i *= 10)
-  //   {
-  //     Console.WriteLine($"  Exporting {i} vertex graphs...");
-
-  //     File.WriteAllText($"assets/{i}-euler", Graph.FillGraph(i, GraphType.euler).Export());
-  //     Console.WriteLine("  ...Euler Done");
-  //     File.WriteAllText($"assets/{i}-semi-euler", Graph.FillGraph(i, GraphType.semiEuler).Export());
-  //     Console.WriteLine("  ...Semi euler Done");
-  //     File.WriteAllText($"assets/{i}-non-euler", Graph.FillGraph(i, GraphType.nonEuler).Export());
-  //     Console.WriteLine("  ...Non euler Done\n");
-  //   }
-
-  //   Console.WriteLine("Finished exporting graphs...");
-  // }
-
   static void Print(String type, int i, Stopwatch stopwatch)
   {
     Console.WriteLine($"{type}:\tTamanho:\t{i}\tTempo:\t{stopwatch.ElapsedMilliseconds}ms");
